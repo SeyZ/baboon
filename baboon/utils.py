9fc47e691e2946928ac9985b96846a966134b514
Raphaël De Giusti <raphael.degiusti@gmail.com>
2012-08-19 00:02:31 +0200

    Added timer function decorator.

--- baboon/utils.py
+++ baboon/utils.py
@@ -1,4 +1,14 @@
+import time
 
+
+def time_me(func):
+    def wrapper(*arg):
+        t1 = time.time()
+        res = func(*arg)
+        t2 = time.time()
+        print '%s took %0.3f s' % (func.func_name, (t2-t1))
+        return res
+    return wrapper
 
 def singleton(cls):
     instances = {}
